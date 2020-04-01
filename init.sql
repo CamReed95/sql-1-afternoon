e4f5508a2ca346480680f23752c22b7593d99fb5
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-01 15:14:44 -0700

    progress1

--- init.sql
+++ init.sql
@@ -1,7 +1,25 @@
 -- Table person
 CREATE TABLE person (
-    
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    email VARCHAR(32),
+    age INTEGER,
+    height INTEGER, 
+    city TEXT,
+    favorite_color TEXT
 )
+
+INSERT INTO person (name, email, age, height, city, favorite_color)
+VALUES
+(1, 'john', 'a@email.com', 1, 1, 'Mesa', 'Blue'),
+(2, 'Jacob', 'b@email.com', 2, 2, 'Phoenix', 'Red'),
+(3, 'Jingle', 'c@email.com', 3, 3, 'Chandler', 'Green'),
+(4, 'Heimer', 'd@email.com', 4, 4, 'Tempe', 'Purple'),
+(5, 'Schmidt', 'e@email.com', 5, 5, 'Gilbert', 'White')
+
+
+
+
 
 /*Create a table called person that records a person's id, name, age, height ( in cm ), city, favorite_color.
 id should be an auto-incrementing id/primary key - Use type: SERIAL
