e431229addfd63e8373428765d2c8cdeff4f495a
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-01 15:29:30 -0700

    finished first section

--- init.sql
+++ init.sql
@@ -11,29 +11,22 @@ CREATE TABLE person (
 
 INSERT INTO person (name, email, age, height, city, favorite_color)
 VALUES
-(1, 'john', 'a@email.com', 1, 1, 'Mesa', 'Blue'),
-(2, 'Jacob', 'b@email.com', 2, 2, 'Phoenix', 'Red'),
-(3, 'Jingle', 'c@email.com', 3, 3, 'Chandler', 'Green'),
-(4, 'Heimer', 'd@email.com', 4, 4, 'Tempe', 'Purple'),
-(5, 'Schmidt', 'e@email.com', 5, 5, 'Gilbert', 'White')
+('john', 'a@email.com', 1, 1, 'Mesa', 'Blue'),
+('Jacob', 'b@email.com', 2, 2, 'Phoenix', 'Red'),
+('Jingle', 'c@email.com', 3, 3, 'Chandler', 'Green'),
+('Heimer', 'd@email.com', 4, 4, 'Tempe', 'Purple'),
+('Schmidt', 'e@email.com', 5, 5, 'Gilbert', 'White')
 
+SELECT * FROM person ORDER BY DESC height;
+SELECT * FROM person ORDER BY ASC height;
+SELECT * FROM person ORDER BY DESC age;
+SELECT * FROM person WHERE age > 20;
+SELECT * FROM person WHERE age = 18;
+SELECT * FROM person WHERE age < 20 AND age > 30;
+SELECT * FROM person WHERE age != 27;
+SELECT * FROM person WHERE favorite_color != 'Red';
+SELECT * FROM person WHERE favorite_color != 'Red' AND favorite_color != 'Blue';
+SELECT * FROM person WHERE favorite_color = 'Orange' OR 'Green';
+SELECT * FROM person WHERE favorite_color IN('Orange', 'Green', 'Blue')
+SELECT * FROM person WHERE favorite_color IN ('Yellow', 'Purple')
 
-
-
-
-/*Create a table called person that records a person's id, name, age, height ( in cm ), city, favorite_color.
-id should be an auto-incrementing id/primary key - Use type: SERIAL
-Add 5 different people into the person database.
-Remember to not include the person_id because it should auto-increment.
-List all the people in the person table by height from tallest to shortest.
-List all the people in the person table by height from shortest to tallest.
-List all the people in the person table by age from oldest to youngest.
-List all the people in the person table older than age 20.
-List all the people in the person table that are exactly 18.
-List all the people in the person table that are less than 20 and older than 30.
-List all the people in the person table that are not 27 (Use not equals).
-List all the people in the person table where their favorite color is not red.
-List all the people in the person table where their favorite color is not red and is not blue.
-List all the people in the person table where their favorite color is orange or green.
-List all the people in the person table where their favorite color is orange, green or blue (use IN).
-List all the people in the person table where their favorite color is yellow or purple (use IN).*/
