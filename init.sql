bc29000590e60e1642fb46a89effea52cf3f873d
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-01 15:38:33 -0700

    working section 2

--- init.sql
+++ init.sql
@@ -30,3 +30,35 @@ SELECT * FROM person WHERE favorite_color = 'Orange' OR 'Green';
 SELECT * FROM person WHERE favorite_color IN('Orange', 'Green', 'Blue')
 SELECT * FROM person WHERE favorite_color IN ('Yellow', 'Purple')
 
+--------------------------------
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    person_id TEXT,
+    product_name TEXT,
+    product_price TEXT, 
+    quantity INTEGER
+)
+
+INSERT INTO orders (person_id, product_id, product_name, product_price, quantity)
+VALUES
+('James', 1, 'a', 1, 1),
+('Jack', 2, 'b', 2, 2),
+('Ann', 3, 'c', 3, 3),
+('Jill', 4, 'd', 4, 4),
+('Janet', 5, 'e', 5, 5)
+
+
+
+
+/*Table - orders
+Instructions
+Create a table called orders that records: order_id, person_id, product_name, product_price, quantity.
+Add 5 orders to the orders table.
+Make orders for at least two different people.
+person_id should be different for different people.
+Select all the records from the orders table.
+Calculate the total number of products ordered.
+Calculate the total order price.
+Calculate the total order price by a single person_id.*/
+
