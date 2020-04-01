b5c20279e2591ed2ca368b6459fef274550d9713
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-01 15:07:31 -0700

    init commit

---
+++ init.sql
@@ -0,0 +1,21 @@
+-- Table person
+CREATE TABLE person (
+    
+)
+
+/*Create a table called person that records a person's id, name, age, height ( in cm ), city, favorite_color.
+id should be an auto-incrementing id/primary key - Use type: SERIAL
+Add 5 different people into the person database.
+Remember to not include the person_id because it should auto-increment.
+List all the people in the person table by height from tallest to shortest.
+List all the people in the person table by height from shortest to tallest.
+List all the people in the person table by age from oldest to youngest.
+List all the people in the person table older than age 20.
+List all the people in the person table that are exactly 18.
+List all the people in the person table that are less than 20 and older than 30.
+List all the people in the person table that are not 27 (Use not equals).
+List all the people in the person table where their favorite color is not red.
+List all the people in the person table where their favorite color is not red and is not blue.
+List all the people in the person table where their favorite color is orange or green.
+List all the people in the person table where their favorite color is orange, green or blue (use IN).
+List all the people in the person table where their favorite color is yellow or purple (use IN).*/
